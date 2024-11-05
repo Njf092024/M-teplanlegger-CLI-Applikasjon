3c47e2758c8f5207a7575bfb4143899a758b38f4
Njf092024 <Njf092024@gmail.com>
2024-11-05 14:44:14 +0100

    Add Meeting class with properties for participants and date/time

---
+++ classes/Meeting.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Møteplanlegger_CLI_Applikasjon;
+{
+    public class Meeting
+    {
+        public List<string> Participants { get; set; } = new List<string>();
+        public DateTime DateTime { get; set; }
+
+        public Meeting(List<string> participants, DateTime dateTime)
+        {
+            Participants = participants;
+            DateTime = dateTime;
+        }
+
+        public Meeting() { }
+    }
+}
